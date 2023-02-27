beca938cad0f0c8bc06b6ad6a8ca7a6f347d5ca4
GetRektd <lgibb24@aoiths.org>
2023-02-27 12:44:28 -0500

    4.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +18,47 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    private void OnTriggerEnter(Collider other){
+
+        if (other.CompareTag("Powerup")) {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRountine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    IEnumerator PowerupCountdownRountine() {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false; 
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OncollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup) {
+
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+
+
+
+    }
+
+
+
+
+
+    
+
+   // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 }
