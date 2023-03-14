32c19b0f3d3e75a30ed1ad8a689c0246e285817e
GetRektd <lgibb24@aoiths.org>
2023-03-14 12:42:30 -0400

    4.4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -21,5 +21,10 @@ public class Enemy : MonoBehaviour
        - transform.position).normalized;
 
        enemyRb.AddForce(lookDirection * speed); 
+
+       if (transform.position.y < -10) 
+       {
+            Destroy(gameObject);
+        }
     }
 }
